e8a139b22b770daa4629fe65c72afbadb9693f4e
speare15 <sampeare15@gmail.com>
2026-09-06 18:58:53 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
+import { createServer } from "http";
+import { handler } from "./handler";
 
+const port = 3000;
+
+const server = createServer(handler);
+
+server.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// This creates the HTTP server and tells it: "Listen on port 3000 and use 'handler' whenever a request arrives."
